4f66fb53101d13fe00f2bcf8387e81c7879934d2
jeromee <bulosanjerome50@gmail.com>
2025-10-04 08:25:50 +0800

    Adding the Produclist in the application seeting up the starting point of it

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import {
 import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
+import { loader as productLoader } from "./pages/Products";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -32,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/products",
         element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productLoader,
       },
       {
         path: "/products/:id",
---
+++ src/components/ProductsContainer.jsx
@@ -0,0 +1,11 @@
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
+const ProductsContainer = () => {
+  return (
+    <>
+      <ProductsList />
+      <ProductsGrid />
+    </>
+  );
+};
+export default ProductsContainer;
