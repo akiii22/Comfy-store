70e48a7f35031425689b4dfde6596185a01733ae
jeromee <bulosanjerome50@gmail.com>
2025-10-08 19:08:11 +0800

    Finishing the projects container viewer with grid and list layout also finishing the layout for the Filters with select price, range, shipping

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,47 @@
+import { useLoaderData, Form, Link } from "react-router-dom";
+import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
+import FormRange from "./FormRange";
+import FormCheckBox from "./FormCheckBox";
+
 const Filters = () => {
-  return <div>Filters</div>;
+  const { meta } = useLoaderData();
+  console.log(meta);
+  return (
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
+      />
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["a-z", "z-a", "high", "low"]}
+        size="select-sm"
+      />
+      <FormRange name="price" label="select price" size="range-sm" />
+      <FormCheckBox name="shipping" label="free shipping" size="checkbox-sm" />
+      <button type="submit" className="btn btn-primary btn-sm">
+        Search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        Reset
+      </Link>
+    </Form>
+  );
 };
 export default Filters;
