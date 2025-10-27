7f2d6a8b842be316b8bfefc37467c318fff2f0a9
jeromee <bulosanjerome50@gmail.com>
2025-10-27 18:47:47 +0800

    Making the layout of cart page and setting up the Cart total layoout and displaying the total price

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,36 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { CartItemsList, SectionTitle, CartTotals } from "../components";
+CartItemsList;
+useSelector;
+
 const Cart = () => {
-  return <h1 className="text-4xl">Cart</h1>;
+  const user = null;
+  const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+  if (numItemsInCart === 0) {
+    <SectionTitle text="Your cart is empty!" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Shopping Cart" />
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals />
+          {user ? (
+            <Link to="/checkout" className="btn btn-primary btn-block mt-8">
+              Proceed to checkout
+            </Link>
+          ) : (
+            <Link to="/login" className="btn btn-primary btn-block mt-8">
+              Please Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 export default Cart;
